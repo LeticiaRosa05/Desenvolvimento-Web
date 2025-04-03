65699ff5f845c617606a2d8fe91e5acf092c0641
Letícia Rosa <leticiasxrosa@gmail.com>
2025-04-03 09:28:11 -0300

    Criação de estrutura básica de if else com alert e prompt no JS

--- Projeto1/script.js
+++ Projeto1/script.js
@@ -1,2 +1,11 @@
-var nome = prompt('Qual o seu nome?');
+var nome = prompt('Qual seu nome?');
 alert('Olá ' + nome + ', seja bem-vindo(a)!');
+
+var idade = prompt('Qual sua idade?');
+if (idade < 18) {
+    alert('Você é menor de idade!');
+} else if (idade >= 18 && idade < 60) {
+    alert('Você é adulto!');
+} else {
+    alert('Você é idoso!');
+}
