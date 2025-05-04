05193ffa7cb23c0612a601cb325f93c77d139360
Letícia Rosa <leticiasxrosa@gmail.com>
2025-05-04 17:24:35 -0300

    Reformulação da função do JavaScript

--- Site-automotivo/script-Carros.js
+++ Site-automotivo/script-Carros.js
@@ -1,43 +1,13 @@
-/*document.getElementById('img').addEventListener('mouseover', transformar);
-document.getElementById('img').addEventListener('mouseout', transformar);*/
+const imagens = document.querySelectorAll('.zoomable'); //pega todas com a classe zoomable
 
-/*function transformar() {
-    const imgCarro = document.getElementById('img');
-    imgCarro.onmouseover = function() {
-        imgCarro.style.transform = "scale(1.1)";
-    }
-    imgCarro.onmouseout = function() {
-        imgCarro.style.transform = "scale(1)";
-    }
-    imgCarro.style.transition = "transform 0.2s ease";
-}*/
+imagens.forEach(img => {
+    img.style.transition = "transform 0.2s ease"; //deixa a transição de tamanhos suave
 
-
-const imgCarro = document.querySelectorAll(".img"); // Seleciona todas as divs
-
-imgCarro.forEach((div, index) => {
-    div.style.transition = "transform 0.2s ease";
-
-    div.addEventListener("mouseover", function() {
-        div.style.transform = "scale(1.1)";
+    img.addEventListener('mouseover', () => {
+        img.style.transform = "scale(1.15)";
     });
 
-    div.addEventListener("mouseout", function() {
-        div.style.transform = "scale(1)";
+    img.addEventListener('mouseout', () => {
+        img.style.transform = "scale(1)";
     });
-
-    console.log(`Efeito aplicado na div: ${index + 1}`); // Confere se todas as divs estão sendo selecionadas
 });
-
-
-/*const imgCarro = document.querySelectorAll(".img");
-
-imgCarro.forEach(img => {
-    img.addEventListener("mouseover", function() {
-        imgCarro.style.transform = "scale(1.1)";
-    });
-    img.addEventListener("mouseout", function() {
-        imgCarro.style.transform = "scale(1)";
-    });
-    imgCarro.style.transition = "transform 0.2s ease";
-});*/
